79af7e4a6f6b3f5e45c30a6eb41fdea080a024e5
Roni Niskanen <roni.niskanen@tuni.fi>
2022-04-14 20:52:15 +0300

    Refactoring script DaylightCycle.cs

--- Assets/_Project/Scripts/DaylightCycle.cs
+++ Assets/_Project/Scripts/DaylightCycle.cs
@@ -33,89 +33,136 @@ public class DaylightCycle : MonoBehaviour
     public AnimationCurve lightingIntensityMultiplier;
     public AnimationCurve reflectionsIntensityMultiplier;
 
-    void Start()
+    private void Start()
     {
         timeRate = 1.0f / fullDayLength;
         time = startTime;
     }
 
-    void Update()
+    private void Update()
     {
-        // Time increment
-        if(timeRunning){
+        if (!timeRunning) return;
         time += timeRate * Time.deltaTime;
-        }
 
-        // Skybox rotation
-        if(time <= 0.5f){
-        RenderSettings.skybox.SetFloat("_Rotation", 360-time*360);
+        SkyBoxRotationBasedOnTime(time);
+
+        ShutdownReflectionOnNight(time);
+
+        SetTimeBackToStart(time);
+
+        SetSunBasedOnTime(time);
+
+        MoonIntensity(time);
+        
+        SetSkyBoxBasedOnTime(time);
+    }
+
+    private void SkyBoxRotationBasedOnTime(float time)
+    {
+        if (time <= 0.5f)
+        {
+            RenderSettings.skybox.SetFloat("_Rotation", 360-time*360);
         }
-        else if(time > 0.5f){
+        else if (time > 0.5f)
+        {
             RenderSettings.skybox.SetFloat("_Rotation", 360-(time-0.5f)*360);
         }
+    }
 
-        // Other lightning
+    private void ShutdownReflectionOnNight(float time)
+    {
         RenderSettings.ambientIntensity = lightingIntensityMultiplier.Evaluate(time);
         RenderSettings.reflectionIntensity
          = reflectionsIntensityMultiplier.Evaluate(time);
-
-        // Set time back to start
-        if(time >= 1.0f){
+    }
+    private void SetTimeBackToStart(float time)
+    {
+        if (time >= 1.0f) 
             time = 0.0f;
-        }
+    }
 
-        // Sun rotation, possibility for moon rotation
-        if(time <= 0.5f){
-            if(time <= 0.25f){
-            sun.transform.eulerAngles = new Vector3(20+time*2*50, 110+time*2*140, 0.0f);
+    private void SetSunBasedOnTime(float time)
+    {
+        SunRotation(time);
+        SunIntensity(time);
+        SunColor(time);
+    }
+
+    private void SunRotation(float time)
+    {
+        if (time <= 0.5f)
+        {
+            if (time <= 0.25f)
+            {
+                sun.transform.eulerAngles = new Vector3(20+time*2*50, 110+time*2*140, 0.0f);
             }
-            else if(time > 0.25f){
+            else if (time > 0.25f)
+            {
                 sun.transform.eulerAngles = new Vector3(80-time*2*70, 100+time*2*160, 0.0f);
             }
+        // Moon rotation is not implemented in this version.
         //} else if (time > 0.5f){
         //    if(time <= 0.75f){
         //    moon.transform.eulerAngles = new Vector3(0+timeMoon*2*90, 90+timeMoon*2*180, 0.0f);
         //    } else if(time > 0.75f){
         //        moon.transform.eulerAngles = new Vector3(90-timeMoon*2*90, 90+timeMoon*2*180, 0.0f);
         //    }
-        } 
-
-        // Sun intensity
-        if(time <= 0.5f){
-        sun.intensity = sunIntensity.Evaluate(time*2);
         }
-        else{
+    }
+
+    private void SunIntensity(float time)
+    {
+        if (time <= 0.5f)
+        {
+            sun.intensity = sunIntensity.Evaluate(time*2);
+        }
+        else 
+        {
             sun.intensity = 0.0f;
         }
+    }
 
-        // Sun color
+    private void SunColor(float time)
+    {
         sun.color = sunColor.Evaluate(time*2);
+    }
 
-        // Moon intensity
-        if(time > 0.5f){
-        moon.intensity = moonIntensity.Evaluate(time*2);
+    private void MoonIntensity(float time)
+    {
+        if (time > 0.5f)
+        {
+            moon.intensity = moonIntensity.Evaluate(time*2);
         }
-        else{
+        else
+        {
             moon.intensity = 0.0f;
         }
+    }
 
-        // Skyboxes
-        if (time <= 0.125f){
+    private void SetSkyBoxBasedOnTime(float time)
+    {
+        if (time <= 0.125f)
+        {
             RenderSettings.skybox = earlyMorningSky;
         }
-        else if(time <= 0.25f){
+        else if (time <= 0.25f)
+        {
             RenderSettings.skybox = morningSky;
         }
-        else if (time <= 0.375f){
+        else if (time <= 0.375f)
+        {
             RenderSettings.skybox = daySky;
         }
-        else if (time <= 0.5f){
+        else if (time <= 0.5f)
+        {
             RenderSettings.skybox = eveningSky;
         }
-        else if (time <= 0.625){
+        else if (time <= 0.625)
+        {
             RenderSettings.skybox = lateEveningSky;
         }
-        else{
+        else
+        {
             RenderSettings.skybox = nightSky;
         }
     }
